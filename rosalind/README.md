6a401aa92ffd8683c20e1801fb7e02a1a672d855
pearcedom <dominic.pearce@ed.ac.uk>
2018-01-09 16:30:57 +0000

    rosalind pcov.py finished
    but whoops, solved it too early and can't yet test it against the real
    data. Must solve previous puzzles first

--- rosalind/README.md
+++ rosalind/README.md
@@ -1,2 +1,1 @@
-# Rosalind
-Rosalind Solutions
+# PUZZLESANDPIE
--- rosalind/python/pcov.py
+++ rosalind/python/pcov.py
@@ -21,32 +21,27 @@ Return: A cyclic superstring of minimal length containing the reads (thus
 corresponding to a candidate cyclic chromosome).
 """
 
+#  ATTAC
+# GATTA
+#   TTACA
+#    TACAG
+#     ACAGA
+#      CAGAT
+#       AGATT
+# GATTACA
 
-
- ATTAC
-   TACAG
-GATTA
-    ACAGA
-     CAGAT
-  TTACA
-      AGATT
-GATTACAGATT
-
-    A T T A C
-  0 0 0 0 0 0 
-G 0 0 0 0 0 0
-A 0 1 0 0 1 0
-T 0 0 2 1 0 0
-T 0 0 1 3 0 0
-A 0 1 0 0 4 0
+#     A T T A C
+#   0 0 0 0 0 0 
+# G 0 0 0 0 0 0
+# A 0 1 0 0 1 0
+# T 0 0 2 1 0 0
+# T 0 0 1 3 0 0
+# A 0 1 0 0 4 0
 
 #create scoring matrix
 def scoring_matrix(nrow, ncol):
     return [[0 for i in range(0, ncol+1)] for j in range(0, nrow+1)]
 
-
-x = kmers[0]
-y = kmers[2]
 def longest_common_substring(x, y):
     len_x = len(x)
     len_y = len(y)
@@ -56,49 +51,41 @@ def longest_common_substring(x, y):
             if x[i] == y[j]:
                 mtx[j+1][i+1] = mtx[j][i] + 1
     #find the highest value and it's position
-    high_val = max(max(mtx))
-    high_row = mtx.index(max(mtx))
+    max_by_row = [max(i) for i in mtx]
+    high_val = max(max_by_row)
+    high_row = max_by_row.index(high_val)
     high_col = mtx[high_row].index(max(mtx[high_row]))
     lcs = x[high_col-high_val:high_col]
     return lcs
 
-##then find the highest within the kmers
-kmers = ["ATTAC", "TACAG", "GATTA", "ACAGA", "CAGAT", "TTACA", "AGATT"]
-pcov = kmers[0]
-del kmers[0]
 
-[len(longest_common_substring(pcov, i)) for i in kmers]
-
-
-
-
-##and combine and repeat with the new pseudo-kmer
-#by findind which ever of...
-pcov.index("ATTA")
-# =0 and adding the other minus "ATTA" to the beginning
-
-
-##I think this is redundent now
-trailing_x0 = high_col-high_val 
-trailing_xend = len_x-high_col 
-
-trailing_y0 = high_row-high_val 
-trailing_yend = len_y-high_row
-
-
-target = "GATTACA"
-
-
-
-
-
-
-
-
-
-
-
-
+def synthesise_kmers(kmers):
+    #take the first kmer
+    pcov = kmers[0]
+    del kmers[0]
+    
+    while len(kmers) > 0:
+        #find the longest lcs within the other kmers
+        best_kmer = ''
+        llcs_len = 0
+        for i in kmers:
+            lcs = longest_common_substring(pcov, i)
+            if len(lcs) > llcs_len:
+                best_kmer = i
+                llcs = lcs
+                llcs_len = len(lcs)
+        #and combine, at the correct end and making sure not to add anything 
+        #that is actually a cyclical repeat
+        pot_rep = best_kmer.replace(llcs, '')
+        if pcov.index(llcs) == 0 and pot_rep != pcov[len(pcov)-len(pot_rep):]:
+            pcov = best_kmer[0:best_kmer.index(llcs)] + pcov
+        elif best_kmer.index(llcs) == 0 and pot_rep != pcov[0:len(pot_rep)]:
+            pcov = pcov[0:pcov.index(llcs)] + best_kmer
+        else:
+            pcov = pcov
+        del kmers[kmers.index(best_kmer)]
+    return pcov
 
 if __name__ == '__main__':
-
+    kmers = ["ATTAC", "TACAG", "GATTA", "ACAGA", "CAGAT", "TTACA", "AGATT"]
+    print(synthesise_kmers(kmers))
