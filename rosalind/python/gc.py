9e0dcc61206b7f34d8363c7d32995b87298c0e8f
pearcedom <dominic.pearce@ed.ac.uk>
2018-01-11 17:06:00 +0000

    gc-content solution

--- rosalind/python/gc.py
+++ rosalind/python/gc.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-
 def read_fasta(path):
     """
     Takes a fasta file and returns a dictionary of ids and sequences.
@@ -9,17 +8,26 @@ def read_fasta(path):
     with open(path, 'r') as fastas:
         f = fastas.read().replace('\n', '').split('>')
         f.pop(0)
-
     data = {}
     for i in f:
-        data[i[:13]] = i[14:]
-        
+        data[i[:13]] = i[13:]
+    return data
 
-def gc_content(fastas):
+def gc_content(s):
+    len_s = len(s)
+    n_g = s.count("G")
+    n_c = s.count("C")
+    gc = ((n_g + n_c) / len_s) * 100
+    return gc
 
-
-
+def get_highest_gc(path):
+    fastas = read_fasta('./data/rosalind_gc.txt')
+    gc_contents = {i: gc_content(fastas[i]) for i in fastas}
+    max_key = max(gc_contents, key = gc_contents.get)
+    return [max_key, gc_contents[max_key]]
 
 if __name__ == '__main__':
+    print(get_highest_gc('./data/rosalind_gc.txt'))
 
-    'data/rosalind_gc.txt'
+    
+
